4bb5ff18cb611d7f8c26be92e41833f8fe7d1e46
Santhanagopalan <sgopalan58@gmail.com>
2022-01-11 23:54:16 -0600

    Added sequelize, registration

--- server/src/app.js
+++ server/src/app.js
@@ -4,19 +4,18 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser');
-
+const {sequelize} = require('./models')
+const config = require('./config/config')
 const app = express()
-
 app.use(morgan('combined'))
 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-    console.log("In server")
-    res.send({
-        message: `Hello! ${req.body.email}! Registered`
-    })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port)
+        console.log(`Server started in ${config.port}`)
+    })
